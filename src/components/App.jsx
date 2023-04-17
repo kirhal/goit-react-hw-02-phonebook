b182d7047f4e87e5937292c80804a1b73c29acd2
kirhal <kirillgaleckiy@gmail.com>
2023-04-17 12:26:20 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,17 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import Section from './section/Section';
 import AddContacts from './phonebook/AddContacts';
 import RenderContacts from './contacts/RenderContacts';
 import FilterContacts from './filter/FilterContacts';
+
+Notify.init({
+  fontSize: '20px',
+  width: '400px',
+  position: 'top-center',
+});
 
 export class App extends Component {
   state = {
@@ -13,12 +20,15 @@ export class App extends Component {
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-5', name: 'Kiki Di', number: '333-91-26' },
+      { id: 'id-6', name: 'Liz Orlan', number: '256-34-85' },
     ],
     filter: '',
   };
 
   addContact = evt => {
     evt.preventDefault();
+    const contacts = this.state.contacts;
     const form = evt.currentTarget;
     const nameValue = form.elements.name.value;
     const numberValue = form.elements.number.value;
@@ -27,15 +37,25 @@ export class App extends Component {
       number: numberValue,
       id: nanoid(),
     };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, currentSubmit],
-    }));
+    if (this.checkOriginalNames(contacts, nameValue)) {
+      Notify.failure('❌ This contact already in');
+    } else {
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, currentSubmit],
+      }));
+    }
     form.reset();
   };
 
   onFilterChange = evt => {
     const filterValue = evt.currentTarget.value.toLowerCase();
     this.setState({ filter: filterValue.trim() });
+  };
+
+  checkOriginalNames = (contacts, contact) => {
+    return contacts.find(
+      ({ name }) => name.toLowerCase() === contact.toLowerCase()
+    );
   };
 
   render() {
@@ -56,9 +76,14 @@ export class App extends Component {
           <AddContacts addContact={this.addContact} />
         </Section>
         <Section title="Contacts">
-          <FilterContacts changeFilter={this.onFilterChange} value={filter} />
-          {contacts.lenght !== 0 && (
-            <RenderContacts contacts={contacts} filter={filter} />
+          {contacts.length !== 0 && (
+            <>
+              <FilterContacts
+                changeFilter={this.onFilterChange}
+                value={filter}
+              />
+              <RenderContacts contacts={contacts} filter={filter} />
+            </>
           )}
         </Section>
       </div>
