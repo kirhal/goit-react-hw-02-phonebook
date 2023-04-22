a0fc39a25f457c24d48903ac7057b080e8c0809a
kirhal <kirillgaleckiy@gmail.com>
2023-04-22 13:43:51 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,16 +18,16 @@ Notify.init({
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Kirlek Dizaered', number: '333-91-26' },
-      { id: 'id-6', name: 'Lizzi Orleans', number: '256-34-85' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  //     { id: 'id-5', name: 'Kirlek Dizaered', number: '333-91-26' },
+  //     { id: 'id-6', name: 'Lizzi Orleans', number: '256-34-85' },
 
   addContact = evt => {
     evt.preventDefault();
@@ -70,6 +70,18 @@ export class App extends Component {
     const { contacts, filter } = this.state;
     return contacts.filter(({ name }) => name.toLowerCase().includes(filter));
   };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentDidMount() {
+    const localContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(localContacts);    
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
 
   render() {
     const { contacts, filter } = this.state;
